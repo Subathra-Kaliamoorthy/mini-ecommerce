e144a237b9d5609f726d159ab9080bab62edca85
Subathra <ksubathra02@gmail.com>
2026-04-15 18:27:07 +0300

    Added backend files

---
+++ backend/controllers/ordercontroller.js
@@ -0,0 +1,14 @@
+const OrderModel = require('../models/orderModel');
+
+//Create order - /api/v1/order
+exports.createOrder = async (req, res, next) =>{
+    const cartItems = req.body;
+    const amount = Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)).toFixed(2);
+    const status = 'pending';
+    const order = await OrderModel.create({cartItems, amount, status})
+    res.json({
+        success: true,
+        message: 'Order works !',
+        order
+    })
+}
---
+++ backend/controllers/productController.js
@@ -0,0 +1,28 @@
+const ProductModel = require('../models/productModel');
+
+// Get Produts API - /api/v1/products
+exports.getProducts = async(req, res, next) => {
+    const products = await ProductModel.find({});
+    res.json({
+        success: true,
+        message : 'Get products working !',
+        products
+    })
+}
+// Get Single Produt API - /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            success: true,
+            message : 'Get single product working !',
+            product
+        })
+    }
+    catch(error){
+        res.status(404).json({
+          success: false,
+          message : 'Unable to get product with the current ID'
+        })
+    }
+}
